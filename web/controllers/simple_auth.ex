47607d829979c6ca89e2a70515c08a432a0efbcb
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-09-24 23:27:20 +0200

    Move login to SimpleAuth

--- web/controllers/simple_auth.ex
+++ web/controllers/simple_auth.ex
@@ -17,6 +17,10 @@ defmodule PasswordlessLoginApp.SimpleAuth do
     assign(conn, :current_user, user)
   end
 
+  def login(conn, user) do
+    conn |> put_session(:user_id, user.id) |> configure_session(renew: true)
+  end
+
   def logout(conn) do
     conn |> configure_session(drop: true)
   end
